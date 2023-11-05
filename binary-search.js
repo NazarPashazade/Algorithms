0f705b7c41b5e374b44b8e0f375ffc79c8b01568
Pashazade <nazar.pashazade@accenture.com>
2023-11-05 18:26:41 +0200

    Binary Search

---
+++ binary-search.js
@@ -0,0 +1,29 @@
+const sortedArray = [15, 55, 57, 69, 74, 95, 98, 99]
+const target = 98;
+
+// T= O(logN)
+
+const binsarySearch = (arr, target) => {
+    let leftIndex = 0
+    let rightIndex = arr.length - 1
+    let middleIndex = Math.round((leftIndex + rightIndex) / 2)
+
+    let isTargetFound = false
+
+    while (!isTargetFound) {
+        if (arr[middleIndex] < target) {
+            middleIndex = Math.round((middleIndex + rightIndex) / 2)
+        } if (arr[middleIndex] > target) {
+            middleIndex = Math.floor((leftIndex + middleIndex) / 2)
+        } else if (target === arr[middleIndex]) {
+            isTargetFound = true
+        }
+    }
+
+
+    if (isTargetFound) {
+        console.log(`Target is found`)
+    }
+}
+
+binsarySearch(sortedArray, target)
